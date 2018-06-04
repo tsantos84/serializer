077bc4f825e3eb8110d649a73588c39ed913eef3
tsantos <tales.augusto.santos@gmail.com>
2018-06-04 08:58:02 -0300

    Added simple reflection driver

---
+++ tests/Metadata/Driver/ReflectionDriverTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of the TSantos Serializer package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\TSantos\Serializer\Metadata\Driver;
+
+use PHPUnit\Framework\TestCase;
+use TSantos\Serializer\Metadata\Driver\ReflectionDriver;
+
+class ReflectionDriverTest extends TestCase
+{
+    /** @test */
+    public function it_should_add_all_properties_to_class_metadata()
+    {
+        $subject = new class {
+            private $id;
+            private $name;
+        };
+
+        $reflection = new \ReflectionClass($subject);
+
+        $driver = new ReflectionDriver();
+
+        $classMetadata = $driver->loadMetadataForClass($reflection);
+        $this->assertCount(2, $classMetadata->propertyMetadata);
+    }
+}
