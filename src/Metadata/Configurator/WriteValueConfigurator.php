1809baf2e843735fe3fede982c616f0fbeda76c3
tsantos <tales.augusto.santos@gmail.com>
2018-06-04 09:16:51 -0300

    Added WriteValue metadata configurator

---
+++ src/Metadata/Configurator/WriteValueConfigurator.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the TSantos Serializer package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TSantos\Serializer\Metadata\Configurator;
+
+use TSantos\Serializer\Metadata\ClassMetadata;
+use TSantos\Serializer\Metadata\ConfiguratorInterface;
+use TSantos\Serializer\Metadata\PropertyMetadata;
+
+/**
+ * Class WriteValueConfigurator
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class WriteValueConfigurator implements ConfiguratorInterface
+{
+    public function configure(ClassMetadata $classMetadata): void
+    {
+        foreach ($classMetadata->propertyMetadata as $propertyMetadata) {
+            if (null !== $propertyMetadata->setter) {
+                continue;
+            }
+
+            $this->doConfigure($classMetadata, $propertyMetadata);
+        }
+    }
+
+    private function doConfigure(ClassMetadata $classMetadata, PropertyMetadata $propertyMetadata): void
+    {
+        $ucName = ucfirst($propertyMetadata->name);
+
+        if ($classMetadata->reflection->hasMethod($setter = 'set' . $ucName)) {
+            $propertyMetadata->setSetter($setter);
+            return;
+        }
+
+        if ($propertyMetadata->reflection->isPublic()) {
+            $propertyMetadata->setter = $propertyMetadata->name;
+        }
+    }
+}
