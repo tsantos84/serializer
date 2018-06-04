c275f9fc263720ba9d0fa825832db82ec13f58e3
tsantos <tales.augusto.santos@gmail.com>
2018-06-04 00:38:49 -0300

    Added getter/isser/hasser configurators

---
+++ src/Metadata/Configurator/ReadValueConfigurator.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of the TSantos Serializer package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TSantos\Serializer\Metadata\Configurator;
+
+use TSantos\Serializer\Metadata\ClassMetadata;
+use TSantos\Serializer\Metadata\ConfiguratorInterface;
+use TSantos\Serializer\Metadata\PropertyMetadata;
+
+/**
+ * Class ReadValueConfigurator
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class ReadValueConfigurator implements ConfiguratorInterface
+{
+    public function configure(ClassMetadata $classMetadata): void
+    {
+        foreach ($classMetadata->propertyMetadata as $propertyMetadata) {
+            if (null !== $propertyMetadata->getter) {
+                continue;
+            }
+
+            $this->doConfigure($classMetadata, $propertyMetadata);
+        }
+    }
+
+    private function doConfigure(ClassMetadata $classMetadata, PropertyMetadata $propertyMetadata): void
+    {
+        $ucName = ucfirst($propertyMetadata->name);
+        $getters = ['get' . $ucName, 'is' . $ucName, 'has' . $ucName];
+        $hasGetter = false;
+
+        foreach ($getters as $getter) {
+            if ($classMetadata->reflection->hasMethod($getter)) {
+                $propertyMetadata->setGetter($getter);
+                break;
+            }
+        }
+
+        if (!$hasGetter && $propertyMetadata->reflection->isPublic()) {
+            $propertyMetadata->getter = $propertyMetadata->name;
+        }
+    }
+}
---
+++ tests/Metadata/Configurator/ReadValueConfiguratorTest.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * This file is part of the TSantos Serializer package.
+ *
+ * (c) Tales Santos <tales.augusto.santos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\TSantos\Serializer\Metadata\Configurator;
+
+use PHPUnit\Framework\TestCase;
+use TSantos\Serializer\Metadata\ClassMetadata;
+use TSantos\Serializer\Metadata\Configurator\ReadValueConfigurator;
+use TSantos\Serializer\Metadata\ConfiguratorInterface;
+use TSantos\Serializer\Metadata\PropertyMetadata;
+
+/**
+ * Class ReadValueConfiguratorTest
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
+ */
+class ReadValueConfiguratorTest extends TestCase
+{
+    /**
+     * @var ConfiguratorInterface
+     */
+    private $configurator;
+
+    public function setup()
+    {
+        $this->configurator = new ReadValueConfigurator();
+    }
+
+    /** @test */
+    public function it_should_not_change_the_getter_if_it_is_already_defined()
+    {
+        $subject = new class {
+            public $id;
+        };
+
+        $classMetadata = $this->createClassMetadata($subject);
+        $classMetadata->addPropertyMetadata($property = new PropertyMetadata($classMetadata->name, 'id'));
+        $property->getter = 'someGetter';
+        $this->configurator->configure($classMetadata);
+        $this->assertEquals('someGetter', $property->getter);
+    }
+
+    /** @test */
+    public function it_can_configure_property_to_read_from_its_own_public_value()
+    {
+        $subject = new class {
+            public $id;
+        };
+
+        $classMetadata = $this->createClassMetadata($subject);
+        $classMetadata->addPropertyMetadata($property = new PropertyMetadata($classMetadata->name, 'id'));
+        $this->configurator->configure($classMetadata);
+        $this->assertEquals('id', $property->getter);
+    }
+
+    /** @test */
+    public function it_should_configure_property_to_ready_from_getter_method()
+    {
+        $subject = new class {
+            private $id;
+            public function getId() {}
+        };
+
+        $classMetadata = $this->createClassMetadata($subject);
+        $classMetadata->addPropertyMetadata($property = new PropertyMetadata($classMetadata->name, 'id'));
+        $this->configurator->configure($classMetadata);
+        $this->assertEquals('getId', $property->getter);
+    }
+
+    /** @test */
+    public function it_should_configure_property_to_ready_from_isser_method()
+    {
+        $subject = new class {
+            private $published;
+            public function isPublished() {}
+        };
+
+        $classMetadata = $this->createClassMetadata($subject);
+        $classMetadata->addPropertyMetadata($property = new PropertyMetadata($classMetadata->name, 'published'));
+        $this->configurator->configure($classMetadata);
+        $this->assertEquals('isPublished', $property->getter);
+    }
+
+    /** @test */
+    public function it_should_configure_property_to_ready_from_hasser_method()
+    {
+        $subject = new class {
+            private $address;
+            public function hasAddress() {}
+        };
+
+        $classMetadata = $this->createClassMetadata($subject);
+        $classMetadata->addPropertyMetadata($property = new PropertyMetadata($classMetadata->name, 'address'));
+        $this->configurator->configure($classMetadata);
+        $this->assertEquals('hasAddress', $property->getter);
+    }
+
+    private function createClassMetadata($subject): ClassMetadata
+    {
+        return new ClassMetadata(get_class($subject));
+    }
+}
